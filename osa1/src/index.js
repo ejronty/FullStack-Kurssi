6157eba5db02c72a9529c5724cabbd8fa1e49bf0
Rönty Eelis J <32396100+ejronty@users.noreply.github.com>
2018-01-27 11:31:58 +0200

    tehtava 1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -31,9 +31,17 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
   return (
     <div>
-      <p>{props.osa1} {props.teht1}</p>
-      <p>{props.osa2} {props.teht2}</p>
-      <p>{props.osa3} {props.teht3}</p>
+      <Osa osa={props.osa1} teht={props.teht1} />
+      <Osa osa={props.osa2} teht={props.teht2} />
+      <Osa osa={props.osa3} teht={props.teht3} />
+    </div>
+  )
+}
+
+const Osa = (props) => {
+  return (
+    <div>
+      <p>{props.osa} {props.teht}</p>
     </div>
   )
 }
