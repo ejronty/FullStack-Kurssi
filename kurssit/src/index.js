584e30904094cb4632c49f1b8a098ea54613d534
Rönty Eelis J <32396100+ejronty@users.noreply.github.com>
2018-02-03 11:21:58 +0200

    Tehtava 2.1

---
+++ kurssit/src/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10,
+        id: 1
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7,
+        id: 2
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id: 3
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
+}
+
+const Kurssi = (props) => {
+  return (
+    <div>
+      <Otsikko kurssi={props.kurssi.nimi} />
+      <Sisalto osat={props.kurssi.osat} />
+      {/*<Yhteensa osat={props.kurssi.osat} />*/}
+    </div>
+  )
+}
+
+const Otsikko = (props) => {
+  return(
+    <div>
+      <h1>{props.kurssi}</h1>
+    </div>
+  )
+}
+
+const Sisalto = (props) => {
+  return (
+    <div>
+      {props.osat.map(osa => <Osa key={osa.id} nimi={osa.nimi} teht={osa.tehtavia} />)}
+    </div>
+  )
+}
+
+const Osa = (props) => {
+  return (
+    <div>
+      <p>{props.nimi} {props.teht}</p>
+    </div>
+  )
+}
+/*
+const Yhteensa = (props) => {
+  return (
+    <div>
+      <p>Yhteensa {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
+    </div>
+  )
+}
+*/
+ReactDOM.render(<App />, document.getElementById('root'));
