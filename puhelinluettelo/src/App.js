0a4c507247847481a5e7716a91feae83e944c26f
Rönty Eelis J <32396100+ejronty@users.noreply.github.com>
2018-02-05 14:50:15 +0200

    Tehtavat 2.9 ja 2.10

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -10,7 +10,8 @@ class App extends React.Component {
         }
       ],
       newName: '',
-      newNumber: ''
+      newNumber: '',
+      filter: ''
     }
   }
 
@@ -20,6 +21,10 @@ class App extends React.Component {
 
   handleNumberChange = (event) => {
     this.setState({newNumber: event.target.value})
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({filter: event.target.value})
   }
 
   addPerson = (event) => {
@@ -52,7 +57,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
+        <h1>Puhelinluettelo</h1>
+        <div>
+          Rajaa näytettäviä: <input value={this.state.filter} onChange={this.handleFilterChange} />
+        </div>
+        <h2>Lisää uusi</h2>
         <form onSubmit={this.addPerson}>
           <div>
             nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
@@ -65,14 +74,25 @@ class App extends React.Component {
           </div>
         </form>
         <h2>Numerot</h2>
-        <table>
-          <tbody>
-            {this.state.persons.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
-          </tbody>
-        </table>
+        <Numerot tila={this.state} />
       </div>
     )
   }
+}
+
+const Numerot = (props) => {
+  const naytettavat = 
+    props.tila.filter === '' ?
+      props.tila.persons : 
+      props.tila.persons.filter(person => person.name.includes(props.tila.filter))
+
+  return (
+    <table>
+      <tbody>
+        {naytettavat.map(person => <Person key={person.name} name={person.name} number={person.number} />)}
+      </tbody>
+    </table>
+  )
 }
 
 const Person = (props) => {
