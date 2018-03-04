4a48f3f6183e3db59de76d4402ba8ddc625279fd
Rönty Eelis J <32396100+ejronty@users.noreply.github.com>
2018-03-04 13:43:01 +0200

    Tehtava 5.6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,45 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+  }
+
+  toggleExpansion = () => {
+    this.setState({ expanded: !this.state.expanded })
+  }
+
+  render() {
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+  
+    if (!this.state.expanded) {
+      return (
+        <div style={blogStyle}>
+          <div onClick={this.toggleExpansion}>{this.props.blog.title} {this.props.blog.author}</div>
+        </div>
+      )
+    } else if (this.state.expanded) {
+      return (
+        <div style={blogStyle}>
+          <div onClick={this.toggleExpansion}>{this.props.blog.title} {this.props.blog.author}</div>
+          <div>{this.props.blog.url}</div>
+          <div>{this.props.blog.likes} likes <button>Like</button></div>
+          <div>Added by: {this.props.blog.user === undefined ? 'Unknown' : this.props.blog.user.name}</div>
+        </div>
+      )
+    }
+  
+  }
+}
 
 export default Blog
